d07efd6d72e7e92029084965b3e6c62cdebee4d2
Vinicius Brum <vinicius@convestconsultoria.com.br>
2018-05-07 18:15:41 -0300

    Working day

--- pyuseful.py
+++ pyuseful.py
@@ -1,6 +1,18 @@
 #! python3
 # pyuseful.py - Useful functions.
 
+import datetime
+
+
+def is_working_day(date, holidays=None):
+    """Checks whether a date is a working date."""
+    holidays = holidays if holidays is not None else list()
+
+    if date.weekday() > 4 or date in holidays:
+        return False
+
+    return True
+
 
 def merge_list_dicts(list_dicts):
     """Merges a list of dictionaries overwriting duplicate keys from
@@ -18,3 +30,17 @@ def sorted_dict(dictionary, by_keys=False, reverse=False):
     index = 0 if by_keys else 1
     return sorted(dictionary.items(), key=lambda item: item[index],
                   reverse=reverse)
+
+
+def working_days_between(from_date, to_date, holidays_list=None):
+    """Returns the number of working days between two dates."""
+    days = 0
+    tmp_date = from_date + datetime.timedelta(days=1)
+
+    while tmp_date <= to_date:
+        if is_working_day(tmp_date, holidays_list):
+            days += 1
+
+        tmp_date = tmp_date + datetime.timedelta(days=1)
+
+    return days
