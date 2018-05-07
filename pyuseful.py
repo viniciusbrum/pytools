90b8d5392bc522c67596c4411d9d947db8635b5e
Vinicius Brum <vinicius@convestconsultoria.com.br>
2018-05-07 17:12:55 -0300

    Sorted representation of a dictionary

--- pyuseful.py
+++ pyuseful.py
@@ -11,3 +11,10 @@ def merge_list_dicts(list_dicts):
         new_dict = {**new_dict, **dictionary}
 
     return new_dict
+
+
+def sorted_dict(dictionary, by_keys=False, reverse=False):
+    """Returns a sorted representation of a dictionary."""
+    index = 0 if by_keys else 1
+    return sorted(dictionary.items(), key=lambda item: item[index],
+                  reverse=reverse)
